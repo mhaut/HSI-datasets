d72ca7dad83b1e2d1645744bfae4bb09b865577d
mario <mhaut@users.noreply.github.com>
2020-03-10 10:51:44 +0100

    Update join_dsets.py

--- join_dsets.py
+++ join_dsets.py
@@ -1,20 +1,21 @@
 import shutil
 
 def cat_files(infiles, outfile, buffer=1024):
-	"""
-	infiles: a list of files
-	outfile: the file that will be created
-	buffer: buffer size in bytes
-	"""
-	with open(outfile, 'w+b') as tgt:
-		for infile in infiles:
-			with open(infile, 'r+b') as src:
-				while True:
-					data = src.read(buffer)
-					if data:
-						tgt.write(data)
-					else:
-						break
+    """
+    infiles: a list of files
+    outfile: the file that will be created
+    buffer: buffer size in bytes
+    """
+    with open(outfile, 'w+b') as tgt:
+        for infile in infiles:
+            with open(infile, 'r+b') as src:
+                while True:
+                    data = src.read(buffer)
+                    if data:
+                        tgt.write(data)
+                    else:
+                        break
 cat_files(["./dsetbyparts/houston_part."+str(a) for a in range(10)], "houston.mat")
 cat_files(["./dsetbyparts/KSC_part."+str(a) for a in range(2)], "KSC.mat")
+cat_files(["./dsetbyparts/paviaC_part."+str(a) for a in range(2)], "paviaC.mat")
 shutil.rmtree('./dsetbyparts')
